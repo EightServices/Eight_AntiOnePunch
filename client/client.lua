52c2c2a8714777284266507db35ee372c1fd8f8c
EightServices <146012223+EightServices@users.noreply.github.com>
2024-05-26 13:55:59 +0200

    Add files via upload

---
+++ client/client.lua
@@ -0,0 +1,6 @@
+CreateThread(function()
+    while true do
+        Wait(0)
+        DisableControlAction(0, 140, true)
+    end
+end)
---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Eight Services'
+description 'https://discord.gg/A96G7gZnbS'
+version '1.0.0'
+
+client_scripts {
+'client/client.lua'
+}
